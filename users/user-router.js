1f01e4d66f6c6d08476be0017b4bbfc85ae7a531
vy3191 <venkyafhs@gmail.com>
2020-03-02 23:49:01 -0500

    Added post router get method

--- users/user-router.js
+++ users/user-router.js
@@ -1,8 +1,9 @@
 const express = require("express")
+const postRouter = require('../posts/post-router');
 const db = require("../data/config")
 
 const router = express.Router()
-
+router.use("/:id/posts", postRouter);
 router.get("/", async (req, res, next) => {
 	try {
 		res.json(await db("users"))
